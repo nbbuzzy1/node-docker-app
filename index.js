a2a521fa2addcf3bb79a4d938a5fccea07ac29cd
Nick Buzzy <nickbuzzy@gmail.com>
2022-09-20 20:36:54 -0400

    add mongodb service; connect to mongodb with express app

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const {
+  MONGO_USER,
+  MONGO_PASSWORD,
+  MONGO_IP,
+  MONGO_PORT,
+} = require("./config/config");
 
 const app = express();
 
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+
+const connectWithRetry = () => {
+  mongoose
+    .connect(mongoURL)
+    .then(() => console.log("successfully connected to database"))
+    .catch((e) => {
+      console.log("error:", e);
+      setTimeout(connectWithRetry, 5000);
+    });
+};
+
+connectWithRetry();
+
 app.get("/", (req, res) => {
-  res.send("<h2>Hi there!!</h2>");
+  res.send("<h2>Hi there!</h2>");
 });
 
 const port = process.env.PORT || 3000;
