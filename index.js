b138e08ff1279ee863e01a42e99f029b8f03126b
Nick Buzzy <nickbuzzy@gmail.com>
2022-10-01 20:09:07 -0400

    set up routes, controller, Mongo model

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ const {
   MONGO_PORT,
 } = require("./config/config");
 
+const postRouter = require("./routes/postRoutes");
+
 const app = express();
+
+app.use(express.json());
 
 const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
 
@@ -26,6 +30,8 @@ connectWithRetry();
 app.get("/", (req, res) => {
   res.send("<h2>Hi there!</h2>");
 });
+
+app.use("/api/v1/posts", postRouter);
 
 const port = process.env.PORT || 3000;
 
